fb21f3b891a139fe3671111021146460e248c172
Benedikt Schnatterbeck <benediktms@hey.com>
2021-11-15 08:19:07 +0000

    Use Charka styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,28 +5,36 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { Heading, Text } from "@chakra-ui/layout"
+import { Button, useColorMode } from "@chakra-ui/react"
 
-const IndexPage: React.FC = () => (
-  <Layout>
-    <Seo title="Home" />
-    <Heading>Hi people</Heading>
-    <Text>Welcome to your new Gatsby site.</Text>
-    <Text>Now go build something great.</Text>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <Text>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </Text>
-  </Layout>
-)
+const IndexPage: React.FC = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
+
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <Heading>Hi people</Heading>
+      <Text>Welcome to your new Gatsby site.</Text>
+      <Text>Now go build something great.</Text>
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={300}
+        quality={95}
+        formats={["auto", "webp", "avif"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      />
+      <Text>
+        <Link to="/page-2/">Go to page 2</Link> <br />
+        <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
+        <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
+        <Link to="/using-dsg">Go to "Using DSG"</Link>
+      </Text>
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === "light" ? "Dark" : "Light"}
+      </Button>
+    </Layout>
+  )
+}
 
 export default IndexPage
