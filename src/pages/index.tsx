42e9ff629df90b96e57fad6f845e4576d995546e
Benedikt Schnatterbeck <benediktms@hey.com>
2021-12-11 10:32:42 +0000

    Add prism.js for code block syntax highlighting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import { graphql, Link as ReachLink, useStaticQuery } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
 import { Box, Text } from '@chakra-ui/layout';
-import { HStack, Link } from '@chakra-ui/react';
+import { HStack } from '@chakra-ui/react';
 import { Query } from '../../graphql-types';
 import { PostLink } from '../components/PostLink';
 import { PhotoAvatar } from '../components/PhotoAvatar';
